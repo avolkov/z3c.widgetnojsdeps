be1471d4e9a96c09d611aa704db44ae3fde021eb
Alex Volkov <alex@flamy.ca>
2012-11-26 12:59:48 -0500

    Adding tests for OptionalWidgetFactory.

--- src/z3c/widgetnojsdeps/optdropdown/test_optdropdown.py
+++ src/z3c/widgetnojsdeps/optdropdown/test_optdropdown.py
@@ -1,6 +1,7 @@
 import unittest2 as unittest
 import zope.schema
 from zope.publisher.browser import TestRequest
+from z3c.form import field
 from z3c.schema.optchoice import OptionalChoice
 from z3c.widgetnojsdeps.optdropdown import OptionalDropdownWidgetFactory,\
     OptionalDropdownWidget
@@ -12,13 +13,15 @@ class TestFieldIntegration(unittest.TestCase):
     def setUp(self):
         self.request = TestRequest()
         self.content = Content()
-
-    def test_optional_widget(self):
+    def test_optional_widget_factory(self):
         optchoice = OptionalChoice(
             __name__='occupation',
             title=u'Occupation',
             description=u'The Occupation',
             values=(u'Programmer', u'Designer', u'Project Manager'),
             value_type=zope.schema.TextLine())
-        bound_opt_choice = optchoice.bind(self.content)
+        form.widget(optchoice='z3c.widgetnojsdeps.optdropdown.OptionalDropdownWidget')
+        field_manager = field.Fields(optchoice)
+        #field_manager['occupation'] = OptionalDropdownWidgetFactory
+        #widgetFactory=OptionalDropdownWidgetFactory
         import pdb; pdb.set_trace()
